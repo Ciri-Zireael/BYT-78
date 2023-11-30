2d19876aa6f8e184b9deaf04939ce91b38f71cd0
Ciri-Zireael <ewajastrzebska21@gmail.com>
2023-11-30 12:44:49 +0100

    write test methods for getting the name and the currency of the Bank

--- test/b_Money/BankTest.java
+++ test/b_Money/BankTest.java
@@ -24,12 +24,16 @@ public class BankTest {
 
 	@Test
 	public void testGetName() {
-		fail("Write test case here");
+		assertEquals("SweBank", SweBank.getName());
+		assertEquals("Nordea", Nordea.getName());
+		assertEquals("DanskeBank", DanskeBank.getName());
 	}
 
 	@Test
 	public void testGetCurrency() {
-		fail("Write test case here");
+		assertEquals(SEK, SweBank.getCurrency());
+		assertEquals(SEK, Nordea.getCurrency());
+		assertEquals(DKK, DanskeBank.getCurrency());
 	}
 
 	@Test
